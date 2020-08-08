4d72327a65d80dc43ff3ddc067f9fa7566a295b2
turbo <turbo3136@gmail.com>
2020-08-08 14:05:54 -0400

    _turbo_dashboard_page.py - nevermind, restructure back to the original structure

--- turbo_dash/_turbo_dashboard_page.py
+++ turbo_dash/_turbo_dashboard_page.py
@@ -1,15 +1,22 @@
 from typing import List
 import pandas as pd
+import dash
+import dash_html_components as html
 
 from ._turbo_filter import turbo_filter
 from ._turbo_output import turbo_output
 
 
 class turbo_dashboard_page(object):
-    """Class that helps us organize information so we can create a dashboard page.
+    """Class that helps us organize information and create a dashboard page.
 
-    This class is a glorified dictionary. It helps the user understand different argument
-    options in a more digestible way than a dictionary.
+    This class does most of the heavy lifting within turbo_dash. It creates all the html
+    and all the callbacks that make the dashboard run.
+
+    Methods:
+        html: create the html for this page
+        callback: create the callback for this page
+
     """
 
     def __init__(
@@ -21,7 +28,7 @@ class turbo_dashboard_page(object):
             output_list: List[turbo_output] = None,
             prebuilt_page: str = None,
     ):
-        """Gather info for a Plotly Dash page.
+        """Create a Plotly Dash page.
 
         Args:
             url (:obj: `str`, optional): default `None`, url for this page, applicable to multi-page dashboards
@@ -38,3 +45,32 @@ class turbo_dashboard_page(object):
         self.menu_filter_list = menu_filter_list
         self.output_list = output_list
         self.prebuilt_page = prebuilt_page
+
+    def html(
+            self,
+            template: str,
+    ) -> html.Div:
+        """create the html for this page
+
+        Args:
+            template (:obj: `str`, optional): layout template we want to use. Options include:
+                ['default', 'turbo', 'turbo-dark']
+
+        Returns:
+            dash_html_components.Div
+        """
+        pass
+
+    def callback(
+            self,
+            app: dash.Dash,
+    ) -> bool:
+        """run the callback for this page
+
+        Args:
+            app (dash.Dash): the dash.Dash app object
+
+        Returns:
+            bool: True if successful, raises errors otherwise
+        """
+        pass
