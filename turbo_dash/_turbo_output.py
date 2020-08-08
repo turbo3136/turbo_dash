11582dec8052c076fdece267d2a8644593401753
turbo <turbo3136@gmail.com>
2020-08-08 14:02:53 -0400

    _turbo_output.py - improve typing and add docstring

--- turbo_dash/_turbo_output.py
+++ turbo_dash/_turbo_output.py
@@ -1,8 +1,11 @@
+from typing import List
 
 
 class turbo_output(object):
-    """
+    """Class that helps us organize information so we can create an output.
 
+    This class is a glorified dictionary. It helps the user understand different argument
+    options in a more digestible way than a dictionary.
     """
 
     def __init__(
@@ -11,7 +14,7 @@ class turbo_output(object):
             x: str = None,
             y: str = None,
             z: str = None,
-            input_list: list = None,
+            input_list: List[str] = None,
     ):
         """
 
@@ -20,7 +23,7 @@ class turbo_output(object):
             x (:obj: `str`, optional): default `None`, string representing the x-axis of the output
             y (:obj: `str`, optional): default `None`, string representing the y-axis of the output
             z (:obj: `str`, optional): default `None`, string representing the z-axis of the output
-            input_list (:obj: `list`, optional): default `None`, list of inputs we want to include
+            input_list (:obj: `List[str]`, optional): default `None`, list of inputs we want to include
                 that will update this output
         """
         self.output_type = output_type
