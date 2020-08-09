c17aa2487792f344b0794e11323e2cc53adf1db6
turbo <turbo3136@gmail.com>
2020-08-08 22:46:36 -0400

    _lookups.py - create template lookup dict

---
+++ turbo_dash/_lookups.py
@@ -0,0 +1,10 @@
+_template_lookup = {
+    'turbo': {
+        # header
+        'header_className': 'header',
+        'header_logo_className': 'logo',
+        'header_links_className': 'header-links',
+        'header_link_className': 'header-link',
+        'header_link_current_className': 'header-link-current',
+    },
+}
