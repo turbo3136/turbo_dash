5147afec25f40c5d4133d51ba8e8d74d6f08f7ef
turbo <turbo3136@gmail.com>
2020-08-10 16:17:14 -0400

    _turbo_dashboard_page.py - finally write some code for html, callbacks, and prebuilt pages

--- turbo_dash/_turbo_dashboard_page.py
+++ turbo_dash/_turbo_dashboard_page.py
@@ -5,6 +5,7 @@ import dash_html_components as html
 
 from ._turbo_filter import turbo_filter
 from ._turbo_output import turbo_output
+from ._lookups import _template_lookup
 
 
 class turbo_dashboard_page(object):
@@ -18,6 +19,8 @@ class turbo_dashboard_page(object):
         callback: create the callback for this page
 
     """
+
+    _template_lookup_dict = _template_lookup
 
     def __init__(
             self,
@@ -46,12 +49,22 @@ class turbo_dashboard_page(object):
         self.output_list = output_list
         self.prebuilt_page = prebuilt_page
 
-    def html(
+        # prebuilt page data
+        # @todo: we'll want to allow the user to access this. It should also have an external source
+        self._homepage_img_filepath = '/static/xkcd_homepage.png'
+        self._fourohfour_img_filepath = '/static/xkcd_fourohfour.png'
+
+    def create_html(
             self,
             template: str = None,
             header_html: html.Div = None,
     ) -> html.Div:
         """create the html for this page
+
+        1. if we're using a prebuilt_page, return that page. Else:
+        2. assemble the menu html
+        3. assemble the content html
+        4. wrap the menu and content html into one div and return
 
         Args:
             template (:obj: `str`, optional): layout template we want to use. Options include:
@@ -61,13 +74,38 @@ class turbo_dashboard_page(object):
         Returns:
             dash_html_components.Div
         """
-        pass
+        # 1
+        if self.prebuilt_page:
+            return self._prebuilt_page_html(
+                template=template,
+                prebuilt_page=self.prebuilt_page,
+                header_html=header_html,
+            )
 
-    def callback(
+        # 2
+        menu_html = html.Div(
+            className=self._template_lookup_dict[template]['menu_className'],
+            children=[menu_filter.html(template=template, df=self.df) for menu_filter in self.menu_filter_list],
+        )
+
+        # 3
+        content_html = html.Div(
+            className=self._template_lookup_dict[template]['content_className'],
+            children=[output.html(template=template, df=self.df) for output in self.output_list],
+        )
+
+        # 4
+        menu_and_content_html = html.Div(
+            className=self._template_lookup_dict[template]['menu_and_content_className'],
+            children=[menu_html, content_html],
+        )
+        return html.Div(children=[header_html, menu_and_content_html])
+
+    def callbacks(
             self,
             app: dash.Dash,
     ) -> bool:
-        """run the callback for this page
+        """run all the callbacks for this page
 
         Args:
             app (dash.Dash): the dash.Dash app object
@@ -75,4 +113,57 @@ class turbo_dashboard_page(object):
         Returns:
             bool: True if successful, raises errors otherwise
         """
-        pass
+        for output in self.output_list:
+            output.callback(
+                app=app,
+                df=self.df,
+                menu_filter_list=self.menu_filter_list,
+            )
+
+        return True
+
+    """protected methods"""
+    def _prebuilt_page_html(
+            self,
+            template: str,
+            prebuilt_page: str,
+            header_html: html.Div,
+    ) -> html.Div:
+        """create the html for the prebuilt page
+
+        Args:
+            template:
+            prebuilt_page:
+            header_html:
+
+        Returns:
+            html.Div
+        """
+        if prebuilt_page == 'homepage':
+            return html.Div(
+                children=[
+                    header_html,
+                    html.Div(
+                        className=self._template_lookup_dict[template]['menu_and_content_className'],
+                        style={'text-align': 'center', 'display': 'block'},
+                        children=html.Img(src=self._homepage_img_filepath),
+                    ),
+                ],
+            )
+
+        elif prebuilt_page == '404':
+            return html.Div(
+                children=[
+                    header_html,
+                    html.Div(
+                        className=self._template_lookup_dict[template]['menu_and_content_className'],
+                        style={'text-align': 'center', 'display': 'block'},
+                        children=html.Img(src=self._fourohfour_img_filepath),
+                    ),
+                ],
+            )
+
+        else:
+            raise ValueError(
+                """Unknown prebuilt_page string: {}. Check file ({}) for details.""".format(prebuilt_page, __file__)
+            )
